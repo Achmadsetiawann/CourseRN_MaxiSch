334e1192b0c53debcf9dfb7d0456114eba16f56a
Achmadsetiawann <achmadsetiawannn@gmail.com>
2021-03-10 01:16:19 +0700

    Course 26

--- App.js
+++ App.js
@@ -4,14 +4,32 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding: 50, marginTop: 30 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+    <View style={ styles.root }>
+      <View style={ styles.inputContainer }>
         <TextInput 
           placeholder="Course Goal" 
-          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }}
+          style={ styles.input }
         />
         <Button title="ADD" />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create ({
+  root : {
+    padding: 50, 
+    marginTop: 30
+  },
+  inputContainer : {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center' 
+  },
+  input : {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+});
