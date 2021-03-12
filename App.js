03ee1f74a85e553218a5bca0baca387f97dfa79e
Achmadsetiawann <achmadsetiawannn@gmail.com>
2021-03-13 01:10:57 +0700

    Adding ScrollView on listItem

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
   // Identifikasi React Hook
@@ -29,11 +29,13 @@ export default function App() {
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View>
+      <ScrollView>
         {courseGoals.map(goal => ( 
         <View key={goal} style={styles.listItem}>
           <Text>{goal}</Text>
         </View>
         ))}
+      </ScrollView>
       </View>
     </View>
   );
